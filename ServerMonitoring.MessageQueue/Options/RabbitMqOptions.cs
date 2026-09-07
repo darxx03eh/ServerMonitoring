b8d8d399f143dec67557c0d7b7e824ce8807054e
Mahmoud Darawsheh <darxx03eh@mail.ru>
2026-09-07 10:26:43 +0300

    Add dead letter queue

--- ServerMonitoring.MessageQueue/Options/RabbitMqOptions.cs
+++ ServerMonitoring.MessageQueue/Options/RabbitMqOptions.cs
@@ -8,8 +8,11 @@ public class RabbitMqOptions
     public string Password { get; set; } = "guest";
     public string VirtualHost { get; set; } = "/";
     public string ExchangeName { get; set; } = "server.events";
+    public string? DeadLetterExchangeName { get; set; }
     public bool DurableExchange { get; set; } = true;
     public string? QueueName { get; set; }
+    public string? DeadLetterQueueName { get; set; }
     public bool DurableQueue { get; set; } = true;
     public ushort PrefetchCount { get; set; } = 10;
+    public string? DeadLetterRoutingKey { get; set; }
 }
